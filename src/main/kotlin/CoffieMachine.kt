2b4abf87d8e368f5afe2597fb7283ed661a2d1a2
Jishan Ansari <jishan010@gmail.com>
2023-10-26 20:30:49 +0530

    If applied,this commit will refactor CoffeMachine class

--- src/main/kotlin/CoffieMachine.kt
+++ src/main/kotlin/CoffieMachine.kt
@@ -1,16 +1,41 @@
+import java.util.*
+
 fun main() {
-    val menuMap: MutableMap<Int, Menu> = displayMenu()
-    val resources = Resources();
+
+    val resources = Resources(30, 30, 30)
     while (true) {
         try {
-            print("Place your order: ")
-            val userInput = readln().toInt()
+            println("Welcome to the Coffee Machine!")
+            println("Menu:")
+            val menuMap: MutableMap<Int, Menu> = displayMenu()
+            print("Enter the number of the coffee you'd like (0-2), or type 'exit' to quit: ")
+            val userInput = readln()
 
-            val selectedMenu = menuMap[userInput];
+            if (userInput == "exit") {
+                break
+            }
 
-            println("You have ordered: ${selectedMenu?.id} - ${selectedMenu?.name} - ${selectedMenu?.price}")
+            val selectedMenu = menuMap[userInput.toInt()];
+            println(userInput)
             val checkAvailability = selectedMenu?.let { resources.getMenuAvailability(it) }
-            println(checkAvailability)
+            if (checkAvailability == true) {
+                println("Making ${selectedMenu.name}.....")
+                Thread.sleep(3000)
+                println("Here is your ${selectedMenu.name}. Enjoy!")
+                println()
+                println()
+                Thread.sleep(3000)
+            } else {
+                Thread.sleep(3000)
+                println("${selectedMenu?.name} Cannot be made ! Insufficient Resources")
+                println("Do you want to refill resources ? type 'yes' or 'exit' to quit: ")
+                if (readln() == "exit") {
+                    break
+                } else {
+                    //refill
+                    resources.refill()
+                }
+            }
 
         } catch (e: NumberFormatException) {
             println("Invalid input. Please enter a valid integer.")
@@ -20,16 +45,16 @@ fun main() {
 
 private fun displayMenu(): MutableMap<Int, Menu> {
 
-    val latte = Menu(1, "Latte", 5, 10, 10, 10)
-    val espresso = Menu(2, "Espresso", 20, 20, 20, 20)
-    val cappuccino = Menu(3, "Cappuccino", 30, 30, 30, 30)
+    val latte = Menu(0, "Latte", 4, 10, 10, 10)
+    val espresso = Menu(1, "Espresso", 7, 20, 20, 20)
+    val cappuccino = Menu(2, "Cappuccino", 6, 30, 30, 30)
 
     val menuMap: MutableMap<Int, Menu> = mutableMapOf(
-        1 to latte, 2 to espresso, 3 to cappuccino
+        0 to latte, 1 to espresso, 2 to cappuccino
     )
 
     for ((_, item) in menuMap) {
-        println("${item.id} - ${item.name} - ${item.price}")
+        println("${item.id} - ${item.name} - $${item.price}")
     }
     return menuMap
 }
@@ -43,19 +68,27 @@ data class Menu(
     val requiredWater: Int,
 )
 
-class Resources {
-    private var water: Int = 50
-    private var milk: Int = 50
-    private var coffeeBeans: Int = 50
+class Resources(private var water: Int, private var milk: Int, private var coffeeBeans: Int) {
+    init {
+        if (water > 30 || milk > 30 || coffeeBeans > 30) {
+            println("Max limit exceeded")
+            refill();
+        }
+    }
 
-    fun getMenuAvailability(menuItem: Menu): String {
+    fun refill() {
+        water = 30
+        milk = 30
+        coffeeBeans = 30
+    }
+
+    fun getMenuAvailability(menuItem: Menu): Boolean {
         if (water < menuItem.requiredWater || milk < menuItem.requiredMilk || coffeeBeans < menuItem.requiredCoffeeBeans) {
-
-            return "No sufficient Items , Sorry go  somewhere else !"
+            return false
         }
         milk -= menuItem.requiredMilk
         water -= menuItem.requiredWater
         coffeeBeans -= menuItem.requiredCoffeeBeans
-        return "Your order is ready"
+        return true
     }
 }
